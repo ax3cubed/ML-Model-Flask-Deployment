8145f708adc4b7eee289a670aabfbc4a36cc932d
unknown <ajunior488@gmail.com>
2020-05-31 13:56:29 +0100

    my flask update

--- request.py
+++ request.py
@@ -1,6 +1,6 @@
 import requests
 
 url = 'http://localhost:5000/predict_api'
-r = requests.post(url,json={'experience':2, 'test_score':9, 'interview_score':6})
+r = requests.post(url,json={'ag':2, 'au':9, 'cd':6, 'cu':23,'fe':3,'mn':3,'ni':2,'pb':3})
 
 print(r.json())
